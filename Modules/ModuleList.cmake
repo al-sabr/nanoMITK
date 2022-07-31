a957ee8f66eea205fc0388b3219be6424c96af0d
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2022-07-31 20:49:09 +0200

    Modules CMake files added.

---
+++ Modules/ModuleList.cmake
@@ -0,0 +1,11 @@
+# The entries in the mitk_modules list must be
+# ordered according to their dependencies.
+
+set(MITK_MODULES
+  Core
+  AppUtil
+  Python
+  QtWidgets
+  QtWidgetsExt
+  QtPython
+)
