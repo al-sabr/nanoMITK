4fa8544239b0dd07988292d0b08c9492b3ee0305
al-sabr <e.bekrek@yandex.com>
2022-07-31 22:28:08 +0200

    All plugins added.

---
+++ Plugins/PluginList.cmake
@@ -0,0 +1,15 @@
+
+# Plug-ins must be ordered according to their dependencies
+
+set(MITK_PLUGINS
+
+  org.blueberry.core.runtime:ON
+  org.blueberry.core.expressions:OFF
+  org.blueberry.core.commands:OFF
+  org.blueberry.core.jobs:OFF
+  org.blueberry.ui.qt:OFF
+  org.blueberry.ui.qt.help:ON
+  org.blueberry.ui.qt.log:ON
+  org.blueberry.ui.qt.objectinspector:OFF
+  
+)
